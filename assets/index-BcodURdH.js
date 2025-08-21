d26d865d9db40c0bf2fdd24a620d8f885097186d
__Kautsar_ <muhammadkautsar456@gmail.com>
2025-08-21 12:05:23 +0700

    Add background, animations, and cursor

---
+++ assets/index-BcodURdH.js
@@ -0,0 +1,1 @@
+import{Q as a,R as e,T as t}from"./gestures-fSMxR8Fo.js";import"./index-D7w2Z35B.js";import"./create-proxy-C1OpAsFW.js";const i={renderer:t,...e,...a};var n=i;export{n as default};
--- assets/index-CyLMrI5j.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,R as e,T as t}from"./gestures-DFFmNPTe.js";import"./index-C185ll5y.js";import"./create-proxy-CGBehw2x.js";const i={renderer:t,...e,...a};var n=i;export{n as default};
